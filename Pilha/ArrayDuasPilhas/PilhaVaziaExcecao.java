45885c08223c437e7b0b217ffa2daeb261baa0eb
Davi Linhares <davimedeiroslinhares14@gmail.com>
2025-09-18 15:11:02 +0000

    Início de Atividade

---
+++ Pilha/ArrayDuasPilhas/PilhaVaziaExcecao.java
@@ -0,0 +1,9 @@
+// usamos RunTimeException por não precisar checar: sem usar try/catch
+// "Minha exceção PilhaVaziaExcecao é um tipo especial de RuntimeException"
+// "String" = tipo de texto, "err" = nome da variável
+// "super..." manda mensagem pro sistema de exceções do Java, se n resolver o problema e o programa parar, mostra a mensagem que passei.
+public class PilhaVaziaExcecao extends RunTimeException { 
+    public PilhaVaziaExcecao(String err) {
+        super(err);
+    }
+}
